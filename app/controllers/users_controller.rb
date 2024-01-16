43f458a12f95ec09b8e4a8b8dbfda1c213e54e21
練習あおい <hoyagaoishii@gmail.com>
2024-01-16 02:58:36 +0000

    9章終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+ before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+ before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+ before_action :correct_user, only: [:edit, :update]
+ before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+
 
   def index
     @users = User.paginate(page: params[:page])
@@ -54,6 +55,15 @@ class UsersController < ApplicationController
   end
 
   def update_basic_info
+    if @user.update(basic_info_params)
+    flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+    flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+      respond_to do |format|
+      format.html { redirect_to users_url }
+      format.turbo_stream
+    end
   end
 
   private
@@ -61,6 +71,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
   end
+  
+  def basic_info_params
+    params.require(:user).permit(:department, :basic_time, :work_time)
+  end
+
 
     # beforeフィルター
 
