fe65efafbca88f6e1d6ea39259ea4088ff8e2325
練習あおい <hoyagaoishii@gmail.com>
2024-01-31 07:38:18 +0000

    11章デバッグできない

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,30 +1,34 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
-  
+
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
-  
+
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
-    #出勤時間が未登録であることを判定します。
+    # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
-     if @attendance.update(started_at: Time.current.change(set: 0))
-       flash[:info] = "おはようございます！"
-     else
-       flash[:danger] = UPDATE_ERROR_MSG
-     end
-    elsif @attendance.finished_at.nil?
-      if @attendance.update(finished_at: Time.current.change(set: 0))
-        flash[:info] = "お疲れ様でした！"
+      if @attendance.update(started_at: Time.current.change(sec: 0))
+        flash[:info] = "おはようございます！"
       else
-        fladh[:danger] = UPDATE_ERROR_MSG
-      end   
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
     end
-  redirect_to @user
-  end 
-  
-  def edit_one_month 
-  end  
+    redirect_to @user
+  end
+
+  def edit_one_month
+  end
+
+  def update_one_month
+    debugger
+  end
 end
