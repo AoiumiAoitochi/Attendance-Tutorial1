e08687acd5041d3fdb149c870062449932b2ab21
練習あおい <hoyagaoishii@gmail.com>
2024-02-06 07:44:19 +0000

    番外編２

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
 
